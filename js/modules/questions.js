7ed917f9c1f1515b08b83a85cd6cf20763013a9e
Dan Singer <d_singer@fanshaweonline.ca>
2024-04-08 23:52:09 -0400

    Layout
    Finished layout~

---
+++ js/modules/questions.js
@@ -0,0 +1,156 @@
+export function questions() {
+    const questions = [
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "nm"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "tms"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "bind"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "776"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "few"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "feh"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "ss"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "bb"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "sd"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "sov"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "gothw"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "pr"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "engage"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "fates"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "rd"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "3h"
+        },
+
+        {
+            question: "",
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            data: "awake"
+        },
+    ]
+}
